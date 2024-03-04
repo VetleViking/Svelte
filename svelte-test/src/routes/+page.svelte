a10917668bcaca38323d14015eabc6a7b4e6dff6
VetleViking <123548420+VetleViking@users.noreply.github.com>
2024-03-04 09:27:12 +0100

    started work on the ovn, and added tailwind

---
+++ svelte-test/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import "../app.css";
+    let count = 0;
+    let navn = "Svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>{navn}</p>
+
+<button on:click={() => count += 1}>Click me</button>
+<p>{count}</p>
+
+<a href="/ovn">Ovn</a>
+
---
+++ svelte-test/src/routes/ovn/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import "../../app.css";
+    let temp = 0;
+</script>
+
+<div class="bg-">
+    <p>{temp}</p>
+
+    <button on:click={() => temp + 1 <= 240 ? temp += 1 : 240}>Pluss</button>
+    <button on:click={() => temp - 1 >= 0 ? temp -= 1 : 0}>Minus</button>
+</div> 
+
+<p>test</p>
